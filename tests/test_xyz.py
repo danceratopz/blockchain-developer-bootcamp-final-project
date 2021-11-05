b33b2312a9649dd35dfb26d8468569c2beb859b3
Web3Wannabe <web3wannabe@gmail.com>
2021-11-05 17:27:08 +0100

    tests: add a unit test for transferring an amount larger than the holder's balance

--- tests/test_xyz.py
+++ tests/test_xyz.py
@@ -4,7 +4,7 @@ Python unit tests for XyzCoin.sol.
 import pytest
 
 from brownie import accounts, XyzCoin
-
+import brownie.exceptions
 
 @pytest.fixture(params=[1, 1000])
 def total_supply(request):
@@ -41,25 +41,11 @@ def test_transfer_valid_amount(deploy_address, xyz_contract):
     balance = xyz_contract.balanceOf(receiver_address)
     assert balance == amount
 
-# def test_transfer_invalid_amount(eth_tester, w3, deploy_address, xyz_contract, total_supply):
-#     receiver_address = eth_tester.get_accounts()[1]
-#     initial_balance = xyz_contract.functions.balanceOf(receiver_address)
-#     assert initial_balance == 0
-#     amount = total_supply + 1
-#     with pytest.raises(TransactionFailed) as execinfo:
-#         _ = xyz_contract.functions.transfer(receiver_address,
-#                                             amount).transact({'from': deploy_address})
-#     assert "transfer amount exceeds balance" in str(execinfo.value)
-
-# @pytest.mark.xfail(reason="Experimenting with test reports")
-# def test_fail_for_test_report(xyz_contract, total_supply):
-#     total_supply_under_test = xyz_contract.functions.totalSupply().call()
-#     assert total_supply_under_test == total_supply + 1
-
-# @pytest.mark.skip(reason="Experimenting with test reports")
-# def test_skip_for_test_report(total_supply):
-#     assert 0
-
-# @pytest.mark.xfail(reason="Experimenting with test reports")
-# def test_xfail_for_test_report(total_supply):
-#     assert 0
+def test_transfer_invalid_amount(deploy_address, xyz_contract, total_supply):
+    receiver_address = accounts[1]
+    initial_balance = xyz_contract.balanceOf(receiver_address)
+    assert initial_balance == 0
+    amount = total_supply + 1
+    with pytest.raises(brownie.exceptions.VirtualMachineError) as execinfo:
+        _ = xyz_contract.transfer(receiver_address, amount).transact({'from': deploy_address})
+    assert "transfer amount exceeds balance" in str(execinfo.value)
