f68d7543251fb9194c0334d9c87612f5f2ec79d5
Web3Wannabe <web3wannabe@gmail.com>
2021-11-09 15:52:37 +0100

    feature: use a mapping to store a list of fractionlized nfts

--- contracts/FractionalizeNFT.sol
+++ contracts/FractionalizeNFT.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.3.2/contracts/token/ERC20/ERC20.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.3.2/contracts/token/ERC721/ERC721.sol";
 
@@ -15,44 +14,53 @@ contract FractionalizeNFT is IERC721Receiver {
 
     address public owner = msg.sender;
 
-    struct Item {
+    mapping (uint => FractionalizedNFT) private fracNFTs;
+    uint256 public fracNFTCount = 0;
+
+    struct FractionalizedNFT {
 	string symbol;
         address token_address;
         ERC721 nft;
 	address payable original_owner;
     }
 
-    Item fractionalized_nft;
-
     constructor() {
         owner = msg.sender;
     }
 
     event Received(address, uint);
+    event NftReceived(address);
 
     // https://blog.soliditylang.org/2020/03/26/fallback-receive-split/
     receive() external payable {
         emit Received(msg.sender, msg.value);
     }
+
     fallback() external payable {
         revert();
     }
 
-    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) public returns (bytes4) {
-        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
-    }
-
-    function createERC20(uint256 _initialAmount, string memory _name, string memory _symbol) public returns (address) {
-        ERC20 newToken = (new ERC20Factory)(_name, _symbol, _initialAmount, msg.sender);
-        return address(newToken);
-    }
-
-    function fractionalizeNft(ERC721 nft, uint256 tokenId, uint256 supply, string memory name, string memory symbol) public returns (address) {
-        //nft.approve(address(this), tokenId);
-        //nft.safeTransferFrom(msg.sender, address(this), tokenId);
-        address token_address = createERC20(supply, name, symbol);
-        fractionalized_nft = Item({symbol: symbol, nft: nft, token_address: token_address, original_owner: payable(msg.sender) });
+    function fractionalizeNft(ERC721 nft, uint256 tokenId, string memory name, string memory symbol, uint256 supply) public returns (address) {
+        nft.safeTransferFrom(msg.sender, address(this), tokenId);
+        ERC20 newToken = (new ERC20Factory)(name, symbol, supply, msg.sender);
+        address token_address = address(newToken);
+        fracNFTs[fracNFTCount] = FractionalizedNFT({symbol: symbol, nft: nft, token_address: token_address, original_owner: payable(msg.sender) });
+        fracNFTCount += 1;
         return token_address;
+    }
+
+    function redeem() public {
+        // A holder of the entire corresponding ERC20 supply can send the tokens in order to redeem the NFT.
+    }
+
+    function buyOut() public {
+        // A buyer can buy the NFT as specified by the reserve price by sending ETH to the contract.
+
+    }
+
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) public returns (bytes4) {
+        emit NftReceived(msg.sender);
+        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
     }
 
 }
