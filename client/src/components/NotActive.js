27aa1ff4f1868a4cccbfa794e612d1518702168c
Web3Wannabe <web3wannabe@gmail.com>
2021-11-26 13:32:04 +0100

    frontend: improve text shown when not connected to the expected network

--- client/src/components/NotActive.js
+++ client/src/components/NotActive.js
@@ -6,12 +6,13 @@ const NotActive = () => {
   return (
     <>
       <Text display="block">
-        Please connect an account to the Ropsten Test Network (<a style={{ color: colors.blue }} href="https://ropsten.etherscan.io/" target="blank">
-          ropsten.etherscan.io</a>) to continue.
+      Please change to the "Ropsten Test Network" in Metamask to continue.
       </Text>
       <FaucetLinks />
     </>
   );
 };
+
+ // (<a style={{ color: colors.blue }} href="https://ropsten.etherscan.io/" target="blank"> ropsten.etherscan.io</a>)
 
 export default NotActive;
