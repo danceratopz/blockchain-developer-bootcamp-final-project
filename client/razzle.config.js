9af00f4177a16a529dfb015187f01f69a55da863
Web3Wannabe <web3wannabe@gmail.com>
2021-11-27 11:07:14 +0100

    fix: add a hotfix for #4 to enable deployment on netlify

--- client/razzle.config.js
+++ client/razzle.config.js
@@ -2,10 +2,21 @@ module.exports = {
   options: {
     buildType: 'spa',
     enableBabelCache: false,
+    debug: { // debug flags
+      options: false, // print webpackOptions that will be used in webpack config
+      config: false, // print webpack config
+      nodeExternals: false // print node externals debug info 
+    },    
   },
   modifyWebpackConfig(opts) {
     const config = opts.webpackConfig;
 
+    config.performance = Object.assign({}, {
+      maxAssetSize: 1600000,
+      maxEntrypointSize: 1600000,
+      hints: false
+    })
+    
     // Ignore fs dependencies so we can use winston
     // if (opts.env.target === 'node' && !opts.env.dev) {
     config.node = { fs: 'empty' };
