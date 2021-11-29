ae9a75b9bbebf302c15f46e026bc23d5858332cb
Web3Wannabe <web3wannabe@gmail.com>
2021-11-30 00:03:00 +0100

    frontend: update FractionalizeNFT and TestNFT contract addresses following deployment to Ropsten

--- client/src/hooks/useFractionalizeNft.js
+++ client/src/hooks/useFractionalizeNft.js
@@ -12,7 +12,7 @@ export function useFractionalizeNft() {
       // TODO: Contract address generation is deterministic; this is the address corresponding to the first deployment of the contract. But can we automate it, as indicated aboce?
       setFractionalizeNftAddress("0x60942c0623CCdAd1441aCe477c21E5fFb93A4D38");
     } else if (chainId === 3) {
-      setFractionalizeNftAddress("0xB782c58a2C277b75F1B8466FEDAF51B17298dD43");
+      setFractionalizeNftAddress("0xEBd4F1bB0C736f98FF6ED83007AF089f2f2b2517");
     }
   }, [chainId]);
 
