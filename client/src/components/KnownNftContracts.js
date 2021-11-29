ace85988c7fe5904a7d8abf3a71ad4e418c9804d
Web3Wannabe <web3wannabe@gmail.com>
2021-11-29 21:03:11 +0100

    frontend: add the address of the new paradigm multifaucet deployment to known nft contracts

--- client/src/components/KnownNftContracts.js
+++ client/src/components/KnownNftContracts.js
@@ -1,9 +1,9 @@
 export const KnownNftContracts = () => {
   return (
       <datalist id="knownNftContracts">
-      <option value="0x4c153bfad26628bdbafecbcd160a0790b1b8f212">Paradigm Faucet- 0x4c153bfad26628bdbafecbcd160a0790b1b8f212</option>
-      <option value="0x3f4d004805BcfB46b225b37f5900F1291b227716">TestNFT - 0x3f4d004805BcfB46b225b37f5900F1291b227716</option>      
+      <option value="0xf5de760f2e916647fd766b4ad9e85ff943ce3a2b">Paradigm Faucet (>2021-11-18)- 0xf5de760f2e916647fd766b4ad9e85ff943ce3a2b</option>
+      <option value="0x4c153bfad26628bdbafecbcd160a0790b1b8f212">Paradigm Faucet (>2021-11-08)- 0x4c153bfad26628bdbafecbcd160a0790b1b8f212</option>
+      <option value="0x3f4d004805BcfB46b225b37f5900F1291b227716">TestNFT - 0x3f4d004805BcfB46b225b37f5900F1291b227716</option>
       </datalist>
   )
 };
-  
