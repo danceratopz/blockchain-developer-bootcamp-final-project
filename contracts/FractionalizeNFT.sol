135502400db441aa7f7f0999b68d63d6f235bf43
Web3Wannabe <web3wannabe@gmail.com>
2021-11-29 20:42:10 +0100

    chore: specify exact solidity compiler version in pragma

--- contracts/FractionalizeNFT.sol
+++ contracts/FractionalizeNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
