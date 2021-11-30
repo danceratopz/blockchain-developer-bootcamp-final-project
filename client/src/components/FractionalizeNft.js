a31248cfcc74b0af4e9d53571926664839e4ff29
Web3Wannabe <web3wannabe@gmail.com>
2021-11-30 16:58:51 +0100

    frontend: clean-up, remove commented out code

--- client/src/components/FractionalizeNft.js
+++ client/src/components/FractionalizeNft.js
@@ -158,18 +158,6 @@ const FractionalizeNft = ({ fractionalizeNftAddress }) => {
     }
   };
 
-  // Respond to 'Fractionalized' event emitted from smart contract.
-  contract.on("Fractionalized", (address, fracNftId) => {
-    console.log("Got fractionalized event! ", address, " ", BigNumber.from(fracNftId).toNumber())
-    if (address != account) {
-      console.log("Event address did not match the logged in account")
-      return;
-    }
-    // setNewlyCreatedFracNftId(fracNftId);
-    // setNewlyCreatedFracNftErc20Address();
-    // setNewlyCreatedFracNftErc20Symbol(fracNftId);
-  });
-
   if (!active) return <Redirect to="/" />;
 
   const { LOADING, WAITING, READY, APPROVED, FRACTIONALIZED, ERROR } = InteractionState;
