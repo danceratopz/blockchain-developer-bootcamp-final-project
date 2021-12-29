5485a6beadfeeaef320f46f35ff236eb01e63d3f
Web3Wannabe <web3wannabe@gmail.com>
2021-12-29 14:25:34 +0100

    style: clean-up and fix typo in comment

--- client/src/hooks/useFractionalizeNft.js
+++ client/src/hooks/useFractionalizeNft.js
@@ -8,8 +8,9 @@ export function useFractionalizeNft() {
 
   useEffect(() => {
     if (chainId === 1337) {
-      //setFractionalizeNftAddress(fractionalizeNftContract.networks[chainId]?.address);
-      // TODO: Contract address generation is deterministic; this is the address corresponding to the first deployment of the contract. But can we automate it, as indicated aboce?
+      // setFractionalizeNftAddress(fractionalizeNftContract.networks[chainId]?.address);
+      // TODO: Contract address generation is deterministic; this is the address corresponding to the first deployment
+      // of the contract. But can we automate it, as indicated above?
       setFractionalizeNftAddress("0x60942c0623CCdAd1441aCe477c21E5fFb93A4D38");
     } else if (chainId === 3) {
       setFractionalizeNftAddress("0xEBd4F1bB0C736f98FF6ED83007AF089f2f2b2517");
