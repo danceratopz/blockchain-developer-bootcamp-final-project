acce8f48a6e8235bafcf4e26f32d1e2af35659f5
danceratopz <danceratopz@gmail.com>
2022-01-08 23:36:39 +0100

    frontend: style; fix react/js formatting

--- client/src/components/FractionalizeNft.js
+++ client/src/components/FractionalizeNft.js
@@ -13,7 +13,7 @@ import { StyledAddress, StyledTxn } from './StyledAddress';
 import { Contract } from '@ethersproject/contracts';
 import { ethers, BigNumber } from "ethers";
 import { shortenAddress } from '../utils/shortenAddress';
-import { TransactionState } from  '../utils/states';
+import { TransactionState } from '../utils/states';
 import { colors } from '../theme';
 import { ConnectBtn, FractFieldset, Legend, FractInput } from './StyledHelpers';
 
@@ -90,7 +90,7 @@ const FractionalizeNft = ({ fractionalizeNftAddress }) => {
       const transaction = await nftContract.approve(
         fractionalizeNftContractAddress,
         nftTokenIndex,
-        {from: account});
+        { from: account });
       console.log("sent")
       const confirmations = chainId === 1337 ? 1 : CONFIRMATION_COUNT;
       await transaction.wait(confirmations);
@@ -102,7 +102,7 @@ const FractionalizeNft = ({ fractionalizeNftAddress }) => {
       setStatus(InteractionState.ERROR);
       if (e.code && typeof e.code === 'number') {
         let message
-        if ( e.hasOwnProperty('data') && e.data.hasOwnProperty('message')) {
+        if (e.hasOwnProperty('data') && e.data.hasOwnProperty('message')) {
           message = "Error calling fractionalizeNFT() - " + e.message + ": " + e.data.message
         } else {
           message = "Error calling fractionalizeNFT() - " + e.message
@@ -131,7 +131,7 @@ const FractionalizeNft = ({ fractionalizeNftAddress }) => {
         erc20Symbol,
         ethers.utils.parseUnits(erc20Supply, 18),  // Our ERC20Factory uses OpenZeppelin's default of 18 digits.
         ethers.utils.parseEther(buyoutPrice),
-        {from: account});
+        { from: account });
       const confirmations = chainId === 1337 ? 1 : CONFIRMATION_COUNT;
       await transaction.wait(confirmations);
       setTxHash(transaction.hash);
@@ -142,7 +142,7 @@ const FractionalizeNft = ({ fractionalizeNftAddress }) => {
       setStatus(InteractionState.ERROR);
       if (e.code && typeof e.code === 'number') {
         let message
-        if ( e.hasOwnProperty('data') && e.data.hasOwnProperty('message')) {
+        if (e.hasOwnProperty('data') && e.data.hasOwnProperty('message')) {
           message = "Error calling fractionalizeNFT() - " + e.message + ": " + e.data.message
         } else {
           message = "Error calling fractionalizeNFT() - " + e.message
@@ -171,136 +171,136 @@ const FractionalizeNft = ({ fractionalizeNftAddress }) => {
         status === FRACTIONALIZED ||
         status === LOADING ||
         status === ERROR) && (
-      <FractFieldset>
-        <legend style={{ float: "left" }}>
-          Fractionalize an NFT
-        </legend>
-        <Text style={{ display: "inline-block" }}>
-          Fractionalize an ERC721 NFT into an ERC20 token - the fractionalizing account receives the total supply.
-        </Text>
-        <FractFieldset>
-          <legend style={{ float: "left" }}>
-            Approve</legend>
-        <Text style={{ display: "inline-block" }}>
-            Approve the FractionalizeNFT contract to take ownership of the NFT.
-          </Text>
-          <br />
-            <FractInput
-           style={ nftContractAddress.length != 42 ? {width: addressInputWidth, border: "1px solid " + colors.red} : {width: addressInputWidth}}
-            name="nftContractAddress"
-            placeholder="NFT contract address (string, 0x...)"
-            type="text"
-            list="knownNftContracts"
-            value={nftContractAddress}
-            onChange={(e) => setNftContractAddress(e.target.value)}
-            />
-          <KnownNftContracts/>
-          <FractInput
-            style={ !isPositiveInteger(nftTokenIndex) ? {width: tokenIdInputWidth, border: "1px solid " + colors.red} : {width: tokenIdInputWidth}}
-            name="nftTokenIndex"
-            placeholder="Token Index (int)"
-            type="text"
-            value={nftTokenIndex}
-            onChange={(e) => setNftTokenIndex(e.target.value)}
-            />
-            {(status != APPROVED && status != ERROR) && (
-              <ConnectBtn
-              style={ !isPositiveInteger(nftTokenIndex) ? {border: "1px solid white", width: "200px"} : {width: "200px"}}
-               disabled={ !isPositiveInteger(nftTokenIndex) }
-               onClick={onApproveNftClick}
-               type="submit"
-               name="approveNft">
-               Approve
-              </ConnectBtn>)}
-          {(status === ERROR) && (
-            <>
-              <ConnectBtn
-              style={ !isPositiveInteger(nftTokenIndex) ? {border: "1px solid white", width: "200px"} : {border: "1px solid " + colors.red, width: "200px"}}
-              disabled={ !isPositiveInteger(nftTokenIndex) }
-               onClick={onApproveNftClick}
-               type="submit"
-               name="approveNft">
-                Approve
-              </ConnectBtn>
-              <Text>(see error below)</Text>
-            </>)}
-             {(status === APPROVED && txHash ) && (
-               <ConnectBtn
-               style={{border: "1px solid " + colors.green, width: "200px"}}
-                    disabled="1"
-                    type="submit"
-                    name="buyNft">
-                      <StyledTxn hash={txHash}/>
-                  </ConnectBtn>)}
-
-        </FractFieldset>
-        <FractFieldset>
-          <legend style={{ float: "left" }}>
-            Fractionalize
-          </legend>
-          <Text style={{ display: "inline-block" }}>
-            Specify the parameters of the ERC20 token that will represent partial ownership of the NFT.
-            <br />
-            The buyout price specifies the price at which the NFT may be bought from the contract by a third party from the Market.
-          </Text>
-          <form>
-            <div>
-              <FractInput
-                style={ erc20Name.length === 0 ? {width: miscInputWidth, border: "1px solid " + colors.red} : {width: miscInputWidth}}
-                name="erc20Name"
-                placeholder="ERC Token Name (string)"
-                type="text"
-                value={erc20Name}
-                onChange={(e) => setErc20Name(e.target.value)}
-              />
-              <FractInput
-                style={ erc20Symbol.length === 0 ? {width: miscInputWidth, border: "1px solid " + colors.red} : {width: miscInputWidth}}
-                name="erc20Symbol"
-                placeholder="ERC Token Symbol (string)"
-                type="text"
-                value={erc20Symbol}
-                onChange={(e) => setErc20Symbol(e.target.value)}
-              />
+          <FractFieldset>
+            <legend style={{ float: "left" }}>
+              Fractionalize an NFT
+            </legend>
+            <Text style={{ display: "inline-block" }}>
+              Fractionalize an ERC721 NFT into an ERC20 token - the fractionalizing account receives the total supply.
+            </Text>
+            <FractFieldset>
+              <legend style={{ float: "left" }}>
+                Approve</legend>
+              <Text style={{ display: "inline-block" }}>
+                Approve the FractionalizeNFT contract to take ownership of the NFT.
+              </Text>
               <br />
               <FractInput
-                style={ !isPositiveInteger(erc20Supply) ? {width: miscInputWidth, border: "1px solid " + colors.red} : {width: miscInputWidth}}
-                name="erc20Supply"
-                placeholder="ERC Token Supply (int)"
+                style={nftContractAddress.length != 42 ? { width: addressInputWidth, border: "1px solid " + colors.red } : { width: addressInputWidth }}
+                name="nftContractAddress"
+                placeholder="NFT contract address (string, 0x...)"
                 type="text"
-                value={erc20Supply}
-                onChange={(e) => setErc20Supply(e.target.value)}
+                list="knownNftContracts"
+                value={nftContractAddress}
+                onChange={(e) => setNftContractAddress(e.target.value)}
               />
-            <FractInput
-                style={ !isPositiveFloat(buyoutPrice) ? {width: miscInputWidth, border: "1px solid " + colors.red} : {width: miscInputWidth}}
-                name="buyoutPrice"
-                placeholder="Buyout Price (Ether)"
+              <KnownNftContracts />
+              <FractInput
+                style={!isPositiveInteger(nftTokenIndex) ? { width: tokenIdInputWidth, border: "1px solid " + colors.red } : { width: tokenIdInputWidth }}
+                name="nftTokenIndex"
+                placeholder="Token Index (int)"
                 type="text"
-                value={buyoutPrice}
-                onChange={(e) => setBuyoutPrice(e.target.value)}
-            />
-            { (status != FRACTIONALIZED) && (
-            <ConnectBtn
-          style={ !isPositiveInteger(nftTokenIndex) || erc20Name.length === 0 || erc20Symbol.length === 0 || !isPositiveInteger(erc20Supply) || !isPositiveFloat(buyoutPrice) ? {width: "200px", border: "1px solid white" } : {width: "200px"} }
-          disabled={ !isPositiveInteger(nftTokenIndex) || erc20Name.length === 0 || erc20Symbol.length === 0 || !isPositiveInteger(erc20Supply) || !isPositiveFloat(buyoutPrice) }
-                onClick={onFractionalizeNftClick}
-                type="submit"
-                name="fractionalize">
-                Fractionalize
-              </ConnectBtn>)}
-          { (status === FRACTIONALIZED ) && txHash && (
+                value={nftTokenIndex}
+                onChange={(e) => setNftTokenIndex(e.target.value)}
+              />
+              {(status != APPROVED && status != ERROR) && (
                 <ConnectBtn
-              style={{border: "1px solid " + colors.green, width: "200px"}}
-              onClick={onFractionalizeNftClick}
-              disabled="1"
-                type="submit"
-                name="fractionalize">
-                <StyledTxn hash={txHash}/>
-              </ConnectBtn>)}
-            </div>
-          </form>
-        </FractFieldset>
+                  style={!isPositiveInteger(nftTokenIndex) ? { border: "1px solid white", width: "200px" } : { width: "200px" }}
+                  disabled={!isPositiveInteger(nftTokenIndex)}
+                  onClick={onApproveNftClick}
+                  type="submit"
+                  name="approveNft">
+                  Approve
+                </ConnectBtn>)}
+              {(status === ERROR) && (
+                <>
+                  <ConnectBtn
+                    style={!isPositiveInteger(nftTokenIndex) ? { border: "1px solid white", width: "200px" } : { border: "1px solid " + colors.red, width: "200px" }}
+                    disabled={!isPositiveInteger(nftTokenIndex)}
+                    onClick={onApproveNftClick}
+                    type="submit"
+                    name="approveNft">
+                    Approve
+                  </ConnectBtn>
+                  <Text>(see error below)</Text>
+                </>)}
+              {(status === APPROVED && txHash) && (
+                <ConnectBtn
+                  style={{ border: "1px solid " + colors.green, width: "200px" }}
+                  disabled="1"
+                  type="submit"
+                  name="buyNft">
+                  <StyledTxn hash={txHash} />
+                </ConnectBtn>)}
+
             </FractFieldset>
-      )}
+            <FractFieldset>
+              <legend style={{ float: "left" }}>
+                Fractionalize
+              </legend>
+              <Text style={{ display: "inline-block" }}>
+                Specify the parameters of the ERC20 token that will represent partial ownership of the NFT.
+                <br />
+                The buyout price specifies the price at which the NFT may be bought from the contract by a third party from the Market.
+              </Text>
+              <form>
+                <div>
+                  <FractInput
+                    style={erc20Name.length === 0 ? { width: miscInputWidth, border: "1px solid " + colors.red } : { width: miscInputWidth }}
+                    name="erc20Name"
+                    placeholder="ERC Token Name (string)"
+                    type="text"
+                    value={erc20Name}
+                    onChange={(e) => setErc20Name(e.target.value)}
+                  />
+                  <FractInput
+                    style={erc20Symbol.length === 0 ? { width: miscInputWidth, border: "1px solid " + colors.red } : { width: miscInputWidth }}
+                    name="erc20Symbol"
+                    placeholder="ERC Token Symbol (string)"
+                    type="text"
+                    value={erc20Symbol}
+                    onChange={(e) => setErc20Symbol(e.target.value)}
+                  />
+                  <br />
+                  <FractInput
+                    style={!isPositiveInteger(erc20Supply) ? { width: miscInputWidth, border: "1px solid " + colors.red } : { width: miscInputWidth }}
+                    name="erc20Supply"
+                    placeholder="ERC Token Supply (int)"
+                    type="text"
+                    value={erc20Supply}
+                    onChange={(e) => setErc20Supply(e.target.value)}
+                  />
+                  <FractInput
+                    style={!isPositiveFloat(buyoutPrice) ? { width: miscInputWidth, border: "1px solid " + colors.red } : { width: miscInputWidth }}
+                    name="buyoutPrice"
+                    placeholder="Buyout Price (Ether)"
+                    type="text"
+                    value={buyoutPrice}
+                    onChange={(e) => setBuyoutPrice(e.target.value)}
+                  />
+                  {(status != FRACTIONALIZED) && (
+                    <ConnectBtn
+                      style={!isPositiveInteger(nftTokenIndex) || erc20Name.length === 0 || erc20Symbol.length === 0 || !isPositiveInteger(erc20Supply) || !isPositiveFloat(buyoutPrice) ? { width: "200px", border: "1px solid white" } : { width: "200px" }}
+                      disabled={!isPositiveInteger(nftTokenIndex) || erc20Name.length === 0 || erc20Symbol.length === 0 || !isPositiveInteger(erc20Supply) || !isPositiveFloat(buyoutPrice)}
+                      onClick={onFractionalizeNftClick}
+                      type="submit"
+                      name="fractionalize">
+                      Fractionalize
+                    </ConnectBtn>)}
+                  {(status === FRACTIONALIZED) && txHash && (
+                    <ConnectBtn
+                      style={{ border: "1px solid " + colors.green, width: "200px" }}
+                      onClick={onFractionalizeNftClick}
+                      disabled="1"
+                      type="submit"
+                      name="fractionalize">
+                      <StyledTxn hash={txHash} />
+                    </ConnectBtn>)}
+                </div>
+              </form>
+            </FractFieldset>
+          </FractFieldset>
+        )}
       {status === LOADING ||
         (status === WAITING && (
           <>
@@ -315,28 +315,28 @@ const FractionalizeNft = ({ fractionalizeNftAddress }) => {
       {status === APPROVED && !!txHash && (
         <>
           <Text style={{ marginTop: '20px', marginBottom: '20px' }}>
-          NFT was successfully approved for transfer in transaction <StyledTxn hash={txHash}/>
+            NFT was successfully approved for transfer in transaction <StyledTxn hash={txHash} />
           </Text>
-          </>
+        </>
       )}
       {status === FRACTIONALIZED && !!txHash && (
         <>
           <Text style={{ marginTop: '20px', marginBottom: '20px' }}>
-          NFT was successfully fractionalized in transaction  <StyledTxn hash={txHash}/>
+            NFT was successfully fractionalized in transaction  <StyledTxn hash={txHash} />
           </Text>
           <Text style={{ marginTop: '20px', marginBottom: '20px' }}>
-          The fractionalized NFT (and its corresponding ERC20 address) can now be found on the <Link style={{ color: colors.blue }} to="/Market">Market</Link> and <Link style={{ color: colors.blue }} to="/Redeem">Redeem</Link> Redeem pages.
+            The fractionalized NFT (and its corresponding ERC20 address) can now be found on the <Link style={{ color: colors.blue }} to="/Market">Market</Link> and <Link style={{ color: colors.blue }} to="/Redeem">Redeem</Link> Redeem pages.
           </Text>
         </>
       )}
       {status === ERROR && (
         <>
           <Text style={{ marginTop: '20px', marginBottom: '20px' }} color={colors.red}>
-          {mmError || 'Unknown error encountered! Please reload.'}
+            {mmError || 'Unknown error encountered! Please reload.'}
           </Text>
         </>
       )}
-      </Container>
+    </Container>
   );
 };
 
