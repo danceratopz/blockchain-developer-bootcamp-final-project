53e8cb3e017ba6b7f6bf617e275d5b48baeec54f
danceratopz <danceratopz@gmail.com>
2022-01-12 08:41:58 +0100

    frontend: add a link to the contract on etherscan to the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,18 +1,23 @@
-import Text from './Text';
-import { Link } from 'react-router-dom';
 import { FaRegCopyright } from 'react-icons/fa';
 import { colors } from '../theme';
+import { StyledAddress } from './StyledAddress';
+import Text from './Text';
 
 const Footer = () => {
+  const ropstenAddress = "0xEBd4F1bB0C736f98FF6ED83007AF089f2f2b2517";
+
   return (
     <div style={{ position: "absolute", bottom: "0", right: "0", margin: "10px", marginTop: "20px" }}>
       <Text block center>
-        <FaRegCopyright color={colors.blue} /> <Link style={{ fontFamily: "Source Code Pro", color: colors.blue }} to={{ pathname: `https://github.com/danceratopz/blockchain-developer-bootcamp-final-project` }} target="_blank">
+        <FaRegCopyright color={colors.blue} />
+        <StyledAnchor href="https://github.com/danceratopz/blockchain-developer-bootcamp-final-project">
           danceratopz
-        </Link>
+        </StyledAnchor>
+        {' - '}
+        <StyledAddress address={ropstenAddress} />
       </Text>
-    </div>
+    </div >
   );
 }
 
-export default Footer;  
+export default Footer;
